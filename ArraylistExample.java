2b14d98988fb2b245b8c3edae6adbf1b3ab12a65
Your name <parthibhank82@gmail.com>
2015-02-17 22:53:19 +0530

    Add ArrayList example

---
+++ ArraylistExample.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+ public class ArraylistExample {
+
+ 	public static void main(String[] args) {
+
+ 		// Declare and initialize ArrayList of integers
+ 		ArrayList<Integer> list = new ArrayList<Integer>();
+
+ 		// Add values to the list
+ 		list.add(50);
+ 		list.add(150);
+ 		list.add(200);
+ 		list.add(250);
+ 		list.add(300);
+
+ 		for(Integer num: list) {
+ 			System.out.println(num)
+ 		}
+
+ 	}
+ }
